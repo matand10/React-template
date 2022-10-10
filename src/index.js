c91d438dfc14df6daf7663e0c19571e9cf501aed
Matan Damary <matandamary10@gmail.com>
2022-10-11 00:16:07 +0300

    Deployed website

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { rootReducer } from './store/root-reducer';
 import { App } from './App';
